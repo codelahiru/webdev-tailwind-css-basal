1961de5b3350e0b25e7746a01bea1d0ff3450173
codelahiru <gayan.hatharasinghe@gmail.com>
2024-01-17 16:08:10 +0530

    011724-16.08

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -3,7 +3,8 @@ import './App.css';
 const App = () => {
   return (
     <div>
-      <section className='h-screen bg-green-100 flex items-center'>
+      <section className='h-screen bg-twitter-blue flex 
+      items-center justify-center m-12 text-center text-3xl p-12 md:p-14 lg:p-16 xl:p-20 '>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
           Illum tenetur, reiciendis vel voluptate deleniti animi, 
           exercitationem iusto amet iure inventore optio eius pariatur! 
@@ -14,51 +15,3 @@ const App = () => {
 }
 
 export default App
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
